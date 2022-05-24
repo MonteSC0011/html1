b6441d1525110b1377fd2b3c0aee16ec5ecce6fb
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-05-24 13:54:05 -0700

    DOM

---
+++ domWalkthrough.js
@@ -0,0 +1,9 @@
+// console.dir(document);
+
+console.log(document.domain); //<--Shows IP Address
+console.log(document.URL);
+
+console.log(document.title);
+console.log(document.doctype);
+
+console.log(document.head);
