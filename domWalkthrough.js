7cd648c59fcbe313b359722de5c5f2a8885f2182
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-05-31 13:44:38 -0700

    JS

--- domWalkthrough.js
+++ domWalkthrough.js
@@ -1,29 +1,70 @@
-// console.dir(document);
+/*
+console.dir(document);
 
-// console.log(document.domain); //<--Shows IP Address
-// console.log(document.URL);
+// Gives the domain of the web page.
+console.log(document.domain);
+// Gives the URL of the web page.
+console.log(document.URL); 
 
-// // 
-// console.log(document.title);
-// // 
-// console.log(document.doctype);
-// // 
-// console.log(document.head);
-// // 
-// console.log(document.body);
-// // 
-// console.log(document.all);
-// // 
-// console.log(document.all[13]);
-// // 
-// document.all[10].textContent = 'We Just Changed The Header';
+THE DOCUMENT ATTRIBUTE CAN ACCESS DIFFERENT ELEMENTS.
 
-// console.log(document.forms);
-// console.log(document.links);
-// console.log(document.images); 
+console.log(document.title);
 
-// ////////////////////////////////////////////////////////////////////////////
+console.log(document.doctype);
 
-// Access the DOM with GETELEMENTBYID 
+console.log(document.head);
 
-console.log(document.getElementById('header-title')); 
+console.log(document.body);
+
+console.log(document.all);
+
+console.log(document.all[10]);
+
+document.all[10].textContent = "I just changed the header!";
+
+Document Object Model can scan the tree for elements. 
+console.log(document.forms);
+console.log(document.links);
+console.log(document.images);
+
+GetElementById.//////////////////
+
+console.log(document.getElementById('header-title'));
+
+// Creation of an object variable using the document function.
+let headerTitle = document.getElementById('header-title');
+
+// 
+headerTitle.textContent = "Im Changing the Header";
+headerTitle.innerText = "I Am Changing It Again.";
+
+headerTitle.style.color = 'hotpink'; 
+
+*/
+
+//////////////////////////////////////////
+/////////////////////////GetElementByClassName///////////
+
+var items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+console.log(items[1]); 
+
+// Styling Property Attributes through the variable.
+
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'pink';
+items[1].textContent = 'Alpha Kappa Alpha';
+items[1].style.color = 'green';
+
+items[0].textContent = "Delta Sigma Theta"
+items[0].style.fontWeight = 'bold';
+items[0].style.backgroundColor = 'red';
+items[0].textContent = 'Oooop - Coleman Love';
+items[0].style.color = 'white';
+
+items[2].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'blue';
+items[2].textContent = 'Zee Phi - Zeta Phi Beta';
+items[2].style.color = 'white';
+
