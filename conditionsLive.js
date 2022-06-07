649c0a9e60c40da6c4f6ba7c98fc4ed137f5fb55
MonteSC0011 <monte.faison@seattlecolleges.edu>
2022-06-07 13:41:18 -0700

    JS

---
+++ conditionsLive.js
@@ -0,0 +1,42 @@
+console.log("Hello World");
+
+
+let randomNumber = Math.random();
+
+console.log(randomNumber); 
+
+// if (randomNumber < 0.5) {
+
+//     console.log("Hello Monte");
+//     console.log(randomNumber);
+
+// }
+
+// if (randomNumber >= 0.5) {
+
+//     console.log('Random Number is greater than 0.5');
+//     console.log(randomNumber);
+
+// }
+
+//If Else Statement:
+
+//
+
+const dayOfWeek = 'Monday';
+
+if (dayOfWeek === 'Monday') {
+
+    console.log('This is Monday');
+
+} else if (dayOfWeek === 'Thursday') {
+
+    console.log("This is Thursday");
+
+} else if (dayOfWeek ===  'Saturday') {
+
+    console.log("Hooray, today is Saturday");
+}
+
+
+
--- domWalkthrough.js
+++ domWalkthrough.js
@@ -143,12 +143,26 @@ headerTitle.style.color = 'hotpink';
 ////////////////////////////////////////////////////////////////////
 //////////QuerySelectorALL
 
+
+/*
+// 
 var titles = document.querySelectorAll('.title');
 console.log(titles);
 titles[0].textContent = 'Hello Monte';  
 
+// 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
 
+// 
+for (var i = 0; i < odd.length; i++) {
 
+    odd[i].style.backgroundColor = 'grey';
+    even[i].style.backgroundColor = 'green';
+    even[i].style.color = 'whitesmoke';
+
+}
+*/
 
 
 
